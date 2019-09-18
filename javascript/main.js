1c072b740def0581ad113f18c7a88faeafafc925
Jarosław Nawrocki <nawrockij@o2.pl>
2019-09-18 18:33:38 +0200

    Add particles.js, modify html structure

--- javascript/main.js
+++ javascript/main.js
@@ -8,3 +8,38 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+// *** Hamburger ***
+
+const navSlide = () => {
+	const hamburger = document.querySelector('.hamburger');
+	const nav = document.querySelector('.menu__item-list');
+	const navLinks = document.querySelectorAll('.menu__item');
+
+	hamburger.addEventListener('click', () => {
+// *** Hamburger - Toggle Nav ***
+		nav.classList.toggle('menu__item-list--is-active');
+
+
+// *** Hamburger - Animate Links ***
+		navLinks.forEach((link, index) => {
+			if(link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.75}s`;
+			}
+		});
+// *** Hamburger Animation ***
+		hamburger.classList.toggle('hamburger--is-active');
+	});
+	
+}
+
+navSlide();
+
+// *** Particles ***
+window.onload = function() {
+  Particles.init({
+    selector: '.banner__canvas'
+  });
+};
