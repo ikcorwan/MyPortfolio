0684d0f5b6aa02d2061249d3afe303fe78063aba
Jarosław Nawrocki <nawrockij@o2.pl>
2020-09-04 16:25:21 +0200

    Add smooth scrolling effect

--- javascript/main.js
+++ javascript/main.js
@@ -5,8 +5,27 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 // We listen to the resize event
 window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+// *** Scroll ***
+window.scroll({
+	top: 2500,
+	left: 0,
+	behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({
+	top: 150, // could be negative value
+	left: 0,
+	behavior: 'smooth'
+});
+
+// Scroll to a certain element
+document.querySelector('.top').scrollIntoView({
+	behavior: 'smooth'
 });
 
 // *** Hamburger ***
@@ -17,29 +36,29 @@ const navSlide = () => {
 	const navLinks = document.querySelectorAll('.menu__item');
 
 	hamburger.addEventListener('click', () => {
-// *** Hamburger - Toggle Nav ***
+		// *** Hamburger - Toggle Nav ***
 		nav.classList.toggle('menu__item-list--is-active');
 
 
-// *** Hamburger - Animate Links ***
+		// *** Hamburger - Animate Links ***
 		navLinks.forEach((link, index) => {
-			if(link.style.animation) {
+			if (link.style.animation) {
 				link.style.animation = '';
 			} else {
 				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.75}s`;
 			}
 		});
-// *** Hamburger Animation ***
+		// *** Hamburger Animation ***
 		hamburger.classList.toggle('hamburger--is-active');
 	});
-	
+
 }
 
 navSlide();
 
 // *** Particles ***
-window.addEventListener("load",function(){
-    Particles.init({
-    	selector: '.banner__canvas'
-  	});
-},false);
+window.addEventListener("load", function () {
+	Particles.init({
+		selector: '.banner__canvas'
+	});
+}, false);
