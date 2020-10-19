1df267f133fb9d69a24c1c87d125c4552dd545f9
Jarosław Nawrocki <nawrockij@o2.pl>
2020-10-19 11:39:56 +0200

    Close the navbar if you click on the link on the mobile version

--- javascript/main.js
+++ javascript/main.js
@@ -9,33 +9,57 @@ window.addEventListener('resize', () => {
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
+
 // *** Hamburger ***
 
-const navSlide = () => {
-	const hamburger = document.querySelector('.hamburger');
-	const nav = document.querySelector('.menu__item-list');
-	const navLinks = document.querySelectorAll('.menu__item');
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.menu__item-list');
+const navItems = document.querySelectorAll('.menu__item');
+const navLinks = document.querySelectorAll('.menu__link');
 
-	hamburger.addEventListener('click', () => {
-		// *** Hamburger - Toggle Nav ***
-		nav.classList.toggle('menu__item-list--is-active');
+let flag = false;
 
-
-		// *** Hamburger - Animate Links ***
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
+// *** Hamburger - Animate Items ***
+const animateItems = () => {
+    navItems.forEach((navItem, index) => {
+			if (navItem.style.animation) {
+				navItem.style.animation = '';
 			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.75}s`;
+				navItem.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.75}s`;
 			}
 		});
-		// *** Hamburger Animation ***
-		hamburger.classList.toggle('hamburger--is-active');
-	});
+}
+
+const navSlide = () => {
+    if (!flag) {
+        flag = !flag;
+        nav.classList.add('menu__item-list--is-active'); 
+        hamburger.classList.add('hamburger--is-active'); 
+        animateItems();
+    } else {
+        flag = !flag;
+        nav.classList.remove('menu__item-list--is-active'); 
+        hamburger.classList.remove('hamburger--is-active'); 
+        animateItems();
+    }
 
 }
 
-navSlide();
+const navClose = () => {
+    if (flag) {
+        flag = false;
+        nav.classList.remove('menu__item-list--is-active'); 
+        hamburger.classList.remove('hamburger--is-active'); 
+        animateItems();
+    }
+}
+
+hamburger.addEventListener('click', navSlide);
+
+for (const navLink of navLinks) {
+    navLink.addEventListener('click', navClose);
+};
+
 
 // *** Particles ***
 window.addEventListener("load", function () {
